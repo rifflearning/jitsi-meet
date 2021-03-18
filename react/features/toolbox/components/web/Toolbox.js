9c2484bfc63bf4e034d454d4d84a6ab0a3f6c127
Uliana Ostrova <uliana@riffanalytics.ai>
2021-03-18 14:23:37 +0200

    minor fix

--- react/features/toolbox/components/web/Toolbox.js
+++ react/features/toolbox/components/web/Toolbox.js
@@ -84,7 +84,6 @@ import OverflowMenuProfileItem from './OverflowMenuProfileItem';
 import ToolbarButton from './ToolbarButton';
 import VideoSettingsButton from './VideoSettingsButton';
 
-window.interfaceConfig.TOOLBAR_BUTTONS = window.interfaceConfig.TOOLBAR_BUTTONS.concat('rifflocalrecording');
 
 /**
  * The type of the React {@code Component} props of {@link Toolbox}.
