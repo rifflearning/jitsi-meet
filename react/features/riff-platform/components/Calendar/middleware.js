a33cad759b4298bc08adc8df86c0c2e8bc7f00d4
Uliana Ostrova <uliana@riffanalytics.ai>
2021-09-17 17:56:36 +0300

    disconnect from calendars on logout

--- react/features/riff-platform/components/Calendar/middleware.js
+++ react/features/riff-platform/components/Calendar/middleware.js
@@ -1,7 +1,11 @@
 
 import { MiddlewareRegistry } from '../../../base/redux';
+import {
+    googleSignOut,
+    microsoftSignOut
+} from '../../actions/calendarSync';
 import { msCalendarSync } from '../../calendarSyncFunctions';
-import { CALENDAR_CLEAR_MS_INTEGRATION, CALENDAR_SET_MS_AUTH_STATE } from '../../constants/actionTypes';
+import { CALENDAR_CLEAR_MS_INTEGRATION, CALENDAR_SET_MS_AUTH_STATE, LOGOUT } from '../../constants/actionTypes';
 
 // eslint-disable-next-line no-unused-vars
 MiddlewareRegistry.register(({ getState, dispatch }) => next => action => {
@@ -14,6 +18,9 @@ MiddlewareRegistry.register(({ getState, dispatch }) => next => action => {
     case CALENDAR_SET_MS_AUTH_STATE:
         msCalendarSync.set(action.msAuthState);
         break;
+    case LOGOUT:
+        dispatch(googleSignOut());
+        dispatch(microsoftSignOut());
     }
 
     return result;
