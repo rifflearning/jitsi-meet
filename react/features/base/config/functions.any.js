d6ddd4f065c13a6c99f0a5387ee500e7c8698cce
Jordan Reedie <jjreedie@gmail.com>
2021-09-27 12:34:24 -0400

    add dynamic riff config

--- react/features/base/config/functions.any.js
+++ react/features/base/config/functions.any.js
@@ -80,6 +80,8 @@ export function getRecordingSharingUrl(state: Object) {
  * be overriding properties.
  * @param {Object} loggingConfig - The loggingConfig Object in which we'll be
  * overriding properties.
+ * @param {Object} riffConfig - The riffConfig Object in which we'll be
+ * overriding properties.
  * @param {Object} json - Object containing configuration properties.
  * Destination object is selected based on root property name:
  * {
@@ -92,11 +94,14 @@ export function getRecordingSharingUrl(state: Object) {
  *     loggingConfig: {
  *         // logging_config.js properties here
  *     }
+ *     riffConfig: {
+ *         // riff_config.js properties here
+ *     }
  * }.
  * @returns {void}
  */
 export function overrideConfigJSON(
-        config: ?Object, interfaceConfig: ?Object, loggingConfig: ?Object,
+        config: ?Object, interfaceConfig: ?Object, loggingConfig: ?Object, riffConfig: ?Object,
         json: Object) {
     for (const configName of Object.keys(json)) {
         let configObj;
@@ -107,6 +112,8 @@ export function overrideConfigJSON(
             configObj = interfaceConfig;
         } else if (configName === 'loggingConfig') {
             configObj = loggingConfig;
+        } else if (configName === 'riffConfig') {
+            configObj = riffConfig;
         }
         if (configObj) {
             const configJSON
@@ -206,6 +213,7 @@ export function restoreConfig(baseURL: string): ?Object {
  * @param {Object} config - This is the general config.
  * @param {Object} interfaceConfig - This is the interface config.
  * @param {Object} loggingConfig - The logging config.
+ * @param {Object} riffConfig - The riff config.
  * @param {URI} location - The new location to which the app is navigating to.
  * @returns {void}
  */
@@ -213,6 +221,7 @@ export function setConfigFromURLParams(
         config: ?Object,
         interfaceConfig: ?Object,
         loggingConfig: ?Object,
+        riffConfig: ?Object,
         location: Object) {
     const params = parseURLParams(location);
     const json = {};
@@ -236,6 +245,7 @@ export function setConfigFromURLParams(
     config && (json.config = {});
     interfaceConfig && (json.interfaceConfig = {});
     loggingConfig && (json.loggingConfig = {});
+    riffConfig && (json.riffConfig = {});
 
     for (const param of Object.keys(params)) {
         let base = json;
@@ -249,7 +259,7 @@ export function setConfigFromURLParams(
         base[last] = params[param];
     }
 
-    overrideConfigJSON(config, interfaceConfig, loggingConfig, json);
+    overrideConfigJSON(config, interfaceConfig, loggingConfig, riffConfig, json);
 }
 
 /* eslint-enable max-params */
---
+++ riff_config.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars, no-var */
+
+// Riff Analytics feature settings
+const riffConfig = {
+    /** access to the riffdata server for sending/receiving meeting metrics */
+    riffdata: {
+        url: '',
+        path: '/api/videodata',
+        email: 'default-user-email',
+        password: 'default-user-password'
+    }
+};
+
+// uncomment this to enable setting riffConfig settings via URL Params
+// window.riffConfig = riffConfig;
+/* eslint-enable no-unused-vars, no-var */
