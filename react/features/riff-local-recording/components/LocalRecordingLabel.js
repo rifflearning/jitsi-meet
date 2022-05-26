59213bbbe0956b7bcc16da7db585dd7474b16191
Jordan Reedie <jjreedie@gmail.com>
2022-05-26 12:15:28 -0400

    isolated changes for local recording to files maintained by riff

---
+++ react/features/riff-local-recording/components/LocalRecordingLabel.js
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import { Label } from '../../base/label/index';
+import { connect } from '../../base/redux';
+import { Tooltip } from '../../base/tooltip';
+
+const LocalRecordingLabel = ({ isEngaged }) => {
+    if (!isEngaged) {
+        return null;
+    }
+
+    return (
+        <Tooltip
+            content = { 'Local recording is engaged' }
+            position = { 'bottom' }>
+            <Label
+                className = 'local-rec-label'
+                text = { 'REC' } />
+        </Tooltip>
+    );
+};
+
+LocalRecordingLabel.propTypes = {
+    isEngaged: PropTypes.bool
+};
+
+const mapStateToProps = state => {
+    return {
+        isEngaged: state['features/riff-local-recording'].isEngaged
+    };
+};
+
+export default connect(mapStateToProps)(LocalRecordingLabel);
