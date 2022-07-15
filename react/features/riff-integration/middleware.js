3d8ca1df4d4a7b51b742742469da41891799ff66
Bala Subrahmanyam Varanasi <vabasu@gmail.com>
2022-07-15 17:21:05 +0530

    remove sibilant integration code

--- react/features/riff-integration/middleware.js
+++ react/features/riff-integration/middleware.js
@@ -15,22 +15,15 @@
  *
  * ******************************************************************************/
 
-import { APP_WILL_MOUNT } from '../base/app';
-import { CONFERENCE_JOINED, CONFERENCE_LEFT } from '../base/conference';
 import { SET_LOCATION_URL } from '../base/connection';
 import { MiddlewareRegistry } from '../base/redux';
-import { TRACK_ADDED } from '../base/tracks';
 import { parseURLParams } from '../base/util';
 
 import {
-    connectToRiffDataServer,
-    riffAddUserToMeeting,
-    riffRemoveUserFromMeeting,
     setRiffMeetingContext,
     setRiffMeetingTitle,
     setRiffParticipantId,
 } from './actions';
-import { attachSibilant } from './functions';
 
 /*
  * Any riff actions that need to be triggered via jitsi state updates
@@ -40,28 +33,6 @@ MiddlewareRegistry.register(store => next => action => {
     const result = next(action);
 
     switch (action.type) {
-    case APP_WILL_MOUNT:
-        // start connection process as soon as the app will mount to ensure
-        // we have a connection before we need it (i.e. when the user clicks 'join')
-        store.dispatch(connectToRiffDataServer());
-        break;
-
-    case TRACK_ADDED:
-        // (re-)attach sibilant whenever a new local audio track is added
-        if (action.track.jitsiTrack.isLocalAudioTrack()) {
-            attachSibilant(action.track.jitsiTrack.getTrack(), store.getState);
-        }
-        break;
-
-    case CONFERENCE_JOINED:
-        store.dispatch(riffAddUserToMeeting());
-        window.addEventListener('beforeunload', _onbeforeunload(store));
-        break;
-
-    case CONFERENCE_LEFT:
-        store.dispatch(riffRemoveUserFromMeeting());
-        window.removeEventListener('beforeunload', _onbeforeunload(store));
-        break;
     case SET_LOCATION_URL:
         // eslint-disable-next-line object-property-newline
         _updateRiffInfoFromUrl(store);
@@ -69,19 +40,6 @@ MiddlewareRegistry.register(store => next => action => {
 
     return result;
 });
-
-
-/**
- * Private function to be called when browser unloads window.
- * Ensures the user is removed from the meeting on the data server.
- *
- * @param {Store} store - The redux store.
- *
- * @returns {void}
- */
-function _onbeforeunload(store) {
-    return () => store.dispatch(riffRemoveUserFromMeeting());
-}
 
 /**
  * Private function to update redux state with values passed in
